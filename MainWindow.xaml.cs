3c509e890d347e16b0117f9f2eb799985911418b
NightPotato <drickman180@gmail.com>
2023-05-10 03:06:22 -0400

    Launch Game & BepInEx Install
    Added functionally it install BepInEx from Github. Basic Launch Game function. Also added ErrorHandler class.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -2,9 +2,11 @@
 using MahApps.Metro.Controls.Dialogs;
 using System.Diagnostics;
 using System.IO;
+using System.IO.Compression;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
+using System.Windows.Media;
 
 namespace MBModManager {
     /// <summary>
@@ -56,7 +58,9 @@ namespace MBModManager {
         }
 
         private void LaunchGame_btn_Click(object sender, System.Windows.RoutedEventArgs e) {
-
+            // Check if all Mods have the required dependencies installed and loaded.
+            // Check if UnityExplorer is installed and loaded else install and load it.
+            Process.Start(clientSettings.GamePath + "\\Mon Bazou.exe");
         }
 
 
@@ -66,26 +70,61 @@ namespace MBModManager {
 
         private async void bepinex_Install_btn_Click(object sender, System.Windows.RoutedEventArgs e) {
 
-            var controller = await this.ShowProgressAsync("Please wait...", "Downloading BepInEx from Github Releases.");
+            MetroDialogSettings dialogSettings = new MetroDialogSettings();
+            dialogSettings.ColorScheme = MetroDialogColorScheme.Inverted;
+            var controller = await this.ShowProgressAsync("Please wait...", "Downloading BepInEx from Github Releases.", false, dialogSettings);
+            controller.SetProgressBarForegroundBrush(new System.Windows.Media.SolidColorBrush(Color.FromRgb(71, 125, 17)));
 
+            string zipPath = System.AppDomain.CurrentDomain.BaseDirectory + "BepInEx.zip";
+
+            if (clientSettings.InternalData == null || clientSettings.InternalData[2] == null) {
+                ErrorHandlers.BIX_INSTALL_FAILED(controller, "Some of the applications internal data is missing, please delete settings.json and relaunch MBModManager.");
+                return;
+            }
+
+            // Check if the GamePath is set in the Options.
+            if (clientSettings.GamePath == null || clientSettings.GamePath == "") {
+                ErrorHandlers.BIX_INSTALL_FAILED(controller, "The Game Path was not set in the options. Please set a Game Path before installing a BepInEx.");
+                return;
+            }
+
+            // GetLatestRelease from Github
             await Task.Delay(2000);
             using (var client = new HttpClient()) {
                 controller.SetProgress(0.25f);
-                var response = await client.GetByteArrayAsync("https://github.com/BepInEx/BepInEx/releases/download/v5.4.21/BepInEx_x64_5.4.21.0.zip");
-                File.WriteAllBytes(System.AppDomain.CurrentDomain.BaseDirectory + "BepInEx.zip", response);
+                var response = await client.GetByteArrayAsync(clientSettings.InternalData[2]);
+                File.WriteAllBytes(zipPath, response);
+                controller.SetProgress(0.35f);
                 controller.SetMessage("Finished Downloading BepInEx.");
             }
 
-            await Task.Delay(3000);
+            // Unzip Release in GamePath
+            await Task.Delay(2000);
             controller.SetMessage("Installing BepInEx to Game Directory.");
             controller.SetProgress(0.45f);
 
-            await Task.Delay(15000);
-            await controller.CloseAsync();
+            try {
+                ZipFile.ExtractToDirectory(zipPath, clientSettings.GamePath);
+            } catch {
+                ErrorHandlers.BIX_INSTALL_FAILED(controller, "Game already contains some of BepInEx files. Please remove these files from your game before installing.");
+                return;
+            }
 
-            // GetLatestRelease from Github
-            // Unzip Release in GamePath
-            // Display Status of Install
+            // Delete BepInEx.zip from AppDirectory
+            await Task.Delay(2000);
+            controller.SetMessage("Cleaning up install files.");
+            File.Delete(zipPath);
+            controller.SetProgress(0.65f);
+
+
+            // Close Install Progress.
+            await Task.Delay(2000);
+
+            controller.SetProgress(0.99999999999f);
+            controller.SetTitle("Success!");
+            controller.SetMessage("We successfully installed BepInEx. Please start the game and finish generating all of BepInEx folders, then you can freely install mods in your game.");
+            await Task.Delay(5000);
+            await controller.CloseAsync();
 
         }
 
