e7db12e22e0af61a7b31f43c14530e50c025357f
NightPotato <drickman180@gmail.com>
2023-05-14 21:52:34 -0400

    Implemented More Design Elements
    Updated the Design of the Mods Section to add more features for mod management.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -106,84 +106,84 @@ namespace MBModManager
             var controller = await this.ShowProgressAsync("Installing Mod!", "Moving mod.zip to working-directory.", false, dialogSettings);
             controller.SetProgressBarForegroundBrush(new SolidColorBrush(Color.FromRgb(71, 125, 17)));
 
-            // Check if GamePath is set
-            if (clientSettings.GamePath == null || clientSettings.GamePath == "Path Not Set") {
-                ErrorHandler.MOD_INSTALL_FAILED(controller, "GamePath was not set in options, please set the GamePath and try again.", false);
-                return;
-            }
+            //// Check if GamePath is set
+            //if (clientSettings.GamePath == null || clientSettings.GamePath == "Path Not Set") {
+            //    ErrorHandler.MOD_INSTALL_FAILED(controller, "GamePath was not set in options, please set the GamePath and try again.", false);
+            //    return;
+            //}
 
-            // Check if Multiple Files were dropped in the install.
-            string[] filePaths = (string[])e.Data.GetData(DataFormats.FileDrop, false);
-            if (filePaths.Length > 1) {
-                ErrorHandler.MOD_INSTALL_FAILED(controller, "Please only drop one mod at a time. We currently do not support installing multiple mods at a time.", false);
-                return;
-            }
-            controller.SetProgress(0.05);
-            await Task.Delay(500);
+            //// Check if Multiple Files were dropped in the install.
+            //string[] filePaths = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+            //if (filePaths.Length > 1) {
+            //    ErrorHandler.MOD_INSTALL_FAILED(controller, "Please only drop one mod at a time. We currently do not support installing multiple mods at a time.", false);
+            //    return;
+            //}
+            //controller.SetProgress(0.05);
+            //await Task.Delay(500);
 
-            // Check if filePaths is null
-            if (filePaths == null) {
-                ErrorHandler.MOD_INSTALL_FAILED(controller, "There is something wrong with the file provided, please try a different file.", false);
-                return;
-            }
-            controller.SetProgress(0.10);
-            await Task.Delay(500);
-
-
-            // Setup workDir for Operations
-            string workDir = System.AppDomain.CurrentDomain.BaseDirectory + "\\workdir";
-            bool workDirExists = Directory.Exists(workDir);
-
-            if (!workDirExists) {
-                Directory.CreateDirectory(workDir);
-            }
-
-            if (workDir == null) {
-                ErrorHandler.MOD_INSTALL_FAILED(controller, "Failed to create Working Directory.", false);
-                return;
-            }
-
-            controller.SetProgress(0.15);
-            await Task.Delay(500);
-
-            // Check if The file dropped is a zip file.
-            if (Path.GetExtension(filePaths[0]) != ".zip") {
-                ErrorHandler.MOD_INSTALL_FAILED(controller, "The file provided was not a valid mod, please try a different file.", false);
-                return;
-            }
-            controller.SetProgress(0.20);
-            await Task.Delay(500);
+            //// Check if filePaths is null
+            //if (filePaths == null) {
+            //    ErrorHandler.MOD_INSTALL_FAILED(controller, "There is something wrong with the file provided, please try a different file.", false);
+            //    return;
+            //}
+            //controller.SetProgress(0.10);
+            //await Task.Delay(500);
 
 
-            // Check if the New File already exists in workdir.
-            string fileName = Path.GetFileName(filePaths[0]);
-            if (File.Exists(workDir + "\\" + fileName)) {
-                ErrorHandler.MOD_INSTALL_FAILED(controller, "The working directory aready contains the file your trying to install. Please cleanup the workDir in the options menu!", false);
-                return;
-            }
+            //// Setup workDir for Operations
+            //string workDir = System.AppDomain.CurrentDomain.BaseDirectory + "\\workdir";
+            //bool workDirExists = Directory.Exists(workDir);
 
-            // Copy the File to WorkDir
-            File.Copy(filePaths[0], workDir + "\\" + fileName);
-            controller.SetProgress(0.24);
-            await Task.Delay(500);
+            //if (!workDirExists) {
+            //    Directory.CreateDirectory(workDir);
+            //}
 
-            // BELOW THIS POINT IF ERROR CLEANUP WORKDIR
+            //if (workDir == null) {
+            //    ErrorHandler.MOD_INSTALL_FAILED(controller, "Failed to create Working Directory.", false);
+            //    return;
+            //}
+
+            //controller.SetProgress(0.15);
+            //await Task.Delay(500);
+
+            //// Check if The file dropped is a zip file.
+            //if (Path.GetExtension(filePaths[0]) != ".zip") {
+            //    ErrorHandler.MOD_INSTALL_FAILED(controller, "The file provided was not a valid mod, please try a different file.", false);
+            //    return;
+            //}
+            //controller.SetProgress(0.20);
+            //await Task.Delay(500);
 
 
-            // Check if mod contains a correct structure.
-            // Create Directory to Extract Files
-            string modDir = Path.GetFileNameWithoutExtension(filePaths[0]);
-            string outputDir = workDir + "\\" + modDir;
-            Directory.CreateDirectory(outputDir);
+            //// Check if the New File already exists in workdir.
+            //string fileName = Path.GetFileName(filePaths[0]);
+            //if (File.Exists(workDir + "\\" + fileName)) {
+            //    ErrorHandler.MOD_INSTALL_FAILED(controller, "The working directory aready contains the file your trying to install. Please cleanup the workDir in the options menu!", false);
+            //    return;
+            //}
 
-            // Unzip mod.zip to ModDir
-            string zipPath = workDir + "\\" + fileName;
-            try {
-                ZipFile.ExtractToDirectory(zipPath, outputDir);
-            } catch {
-                Handlers.ErrorHandler.MOD_INSTALL_FAILED(controller, "Could not extract the mod to working directory, performing cleanup of Working Directory.", true);
-                return;
-            }
+            //// Copy the File to WorkDir
+            //File.Copy(filePaths[0], workDir + "\\" + fileName);
+            //controller.SetProgress(0.24);
+            //await Task.Delay(500);
+
+            //// BELOW THIS POINT IF ERROR CLEANUP WORKDIR
+
+
+            //// Check if mod contains a correct structure.
+            //// Create Directory to Extract Files
+            //string modDir = Path.GetFileNameWithoutExtension(filePaths[0]);
+            //string outputDir = workDir + "\\" + modDir;
+            //Directory.CreateDirectory(outputDir);
+
+            //// Unzip mod.zip to ModDir
+            //string zipPath = workDir + "\\" + fileName;
+            //try {
+            //    ZipFile.ExtractToDirectory(zipPath, outputDir);
+            //} catch {
+            //    Handlers.ErrorHandler.MOD_INSTALL_FAILED(controller, "Could not extract the mod to working directory, performing cleanup of Working Directory.", true);
+            //    return;
+            //}
 
             // Gather ModInfo for the mod being installed and check if it is currently a hosted mod within the API.
             // BackpackMod-19-1-0-3-1659649243.zip
