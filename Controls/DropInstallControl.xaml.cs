db4378b65be2f3bfbab74994a54d0bb2032cb85e
NightPotato <drickman180@gmail.com>
2023-05-16 05:47:47 -0400

    Created DropInstallControl
    This will be reused later when we implement install save files. Right now it only supports Mod installs.

---
+++ Controls/DropInstallControl.xaml.cs
@@ -0,0 +1,29 @@
+﻿using MBModManager.Events;
+using System;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MBModManager.Controls {
+    /// <summary>
+    /// Interaction logic for ModInstallBox.xaml
+    /// </summary>
+    public partial class DropInstallControl : UserControl {
+
+        // TODO: Implement Property to switch between Mod or Save install.
+
+        public DropInstallControl() {
+            InitializeComponent();
+        }
+
+        private void DropInstallControl_DragEnter(object sender, DragEventArgs e) {
+            e.Effects = e.Data.GetDataPresent(DataFormats.FileDrop) ? DragDropEffects.Copy : DragDropEffects.None;
+        }
+
+        private void DropInstallControl_Drop(object sender, DragEventArgs e) {
+            var window = (Application.Current.MainWindow as MainWindow);
+            if (window == null) return;
+            InstallEvents.InstallMod(window, e);
+        }
+    }
+}
